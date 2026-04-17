f02d231375e1dce19af9d968668edc5abbc92600
OscarAvila-123 <oscar.avila-n@uniminuto.edu.co>
2026-04-17 12:06:09 -0500

    Delete Beneficio.java

--- Beneficio.java
+++
@@ -1,9 +0,0 @@
-// Beneficio.java
-// Esta interfaz define comportamientos adicionales que pueden tener diferentes servicios
-// No depende de ninguna jerarquía de herencia, cualquier clase puede implementarla
-
-public interface Beneficio {
-    double aplicarDescuento(double montoBase);
-    boolean validarSubsidio();
-    String obtenerDescripcionBeneficio();
-}
